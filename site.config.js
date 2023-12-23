39b04e062ce0735c755ada53de57b9f8ba516aef
DriedPollack <jihuhw99@gmail.com>
2023-12-23 19:07:44 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "DriedPollack",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/profilePicture1.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
     email: "jihuhw99@gmail.com",
